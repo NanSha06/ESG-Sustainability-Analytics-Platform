98ceab81d128190b833ec773a1985a960d79b268
NanSha06 <146819531+NanSha06@users.noreply.github.com>
2026-06-11 12:54:10 +0530

    database setup and model config

---
+++ app/models/__init__.py
@@ -0,0 +1,23 @@
+from app.models.esg import (
+    Company,
+    DataUpload,
+    EnvironmentalRecord,
+    GovernanceRecord,
+    SocialRecord,
+    ESGScore,
+    DataCategory,
+    ScoreRating,
+    UploadStatus,
+)
+
+__all__ = [
+    "Company",
+    "DataUpload",
+    "EnvironmentalRecord",
+    "SocialRecord",
+    "GovernanceRecord",
+    "ESGScore",
+    "DataCategory",
+    "ScoreRating",
+    "UploadStatus",
+]
